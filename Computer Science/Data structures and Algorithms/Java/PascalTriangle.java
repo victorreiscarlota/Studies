a71b1d84c77fd8a31d9e9594d132ae5768e8508e
victor-010 <victor.reis.carlotta@gmail.com>
2023-10-31 12:17:36 -0300

    Submit of pascal triangle algorithm, still need help with it

--- Computer Science/Data structures and Algorithms/Java/PascalTriangle.java
+++ Computer Science/Data structures and Algorithms/Java/PascalTriangle.java
@@ -1,13 +1,35 @@
+
 /**
  * Exercise: Given an integer numRows, return the first numRows of Pascal's
  * triangle.
  * 
- * In Pascal's triangle, each number is the sum of the two numbers directly
- * above it as shown:
+ * - still can't fully understand the algorithm, feel free to help me understand it with other solutions and/or comments in the code -
  * 
  */
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangulo = new ArrayList<>();
+        if (numRows == 0)
+            return triangulo;
+        List<Integer> primeira_row = new ArrayList<>();
+        primeira_row.add(1);
+        triangulo.add(primeira_row);
 
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> ant_row = triangulo.get(i - 1);
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(ant_row.get(j - 1) + ant_row.get(j));
+            }
+            row.add(1);
+            triangulo.add(row);
+
+        }
+        return triangulo;
     }
+
 }
